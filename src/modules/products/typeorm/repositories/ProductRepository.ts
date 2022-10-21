da310a5fcdbee1d346448ebad2538cddf02a5138
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-20 21:12:21 -0300

    Rotas de produtos

--- src/modules/products/typeorm/repositories/ProductRepository.ts
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -1,6 +1,6 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import Product from '../entities/Product';
-
+@EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
   public async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({
