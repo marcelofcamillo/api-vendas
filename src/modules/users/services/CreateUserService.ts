7bcae972e5fac31bc33152bc674a55d32965016e
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-22 20:35:43 -0300

    Serviço para criação de usuário

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUser {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UserRepository);
+    const emailExists = await userRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used');
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUser;
