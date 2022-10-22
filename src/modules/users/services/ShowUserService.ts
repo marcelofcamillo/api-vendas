5fb19e869bea7f352b6040a1c4f971530e966722
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-22 20:48:55 -0300

    Serviço para exibir um usuário

---
+++ src/modules/users/services/ShowUserService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowUserService {
+  public async execute({ id }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+    const user = await usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    return user;
+  }
+}
+
+export default ShowUserService;
