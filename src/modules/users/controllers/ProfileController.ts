61b13b319cebed42f1ac0b4a9020c75f69bf255e
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-23 20:02:56 -0300

    Rotas de perfil de usuário

--- src/modules/users/controllers/ProfileController.ts
+++ src/modules/users/controllers/ProfileController.ts
@@ -6,7 +6,8 @@ export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const showProfile = new ShowProfileService();
     const user_id = request.user.id;
-    const user = showProfile.execute({ user_id });
+
+    const user = await showProfile.execute({ user_id });
 
     return response.json(user);
   }
@@ -14,7 +15,9 @@ export default class ProfileController {
   public async update(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { name, email, password, old_password } = request.body;
+
     const updateProfile = new UpdateProfileService();
+
     const user = await updateProfile.execute({
       user_id,
       name,
