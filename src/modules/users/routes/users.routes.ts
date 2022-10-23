d11c9fb2c6bdf10ee3bfd902f09ac46e3f8909c4
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-22 21:34:03 -0300

    Rotas de usuários

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,58 @@
+import { celebrate, Segments } from 'celebrate';
+import { Router } from 'express';
+import Joi from 'joi';
+import UsersController from '../controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.update,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
+);
+
+export default usersRouter;
