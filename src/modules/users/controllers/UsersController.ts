4cba742c17a8d0cb7e9737adc5c16b2d4bcb63a8
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-10-22 23:02:19 -0300

    Configuração da lib para uploads - multer

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -10,8 +10,6 @@ export default class UsersController {
     const listUser = new ListUserService();
     const users = await listUser.execute();
 
-    console.log(request.user.id);
-
     return response.json(users);
   }
 
