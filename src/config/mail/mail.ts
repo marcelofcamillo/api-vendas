0e035d017f4b28ad1c69682bdb5807056d20ecac
Marcelo Camillo <marcelocamillo18@gmail.com>
2022-11-19 22:15:18 -0300

    Configurando envio de email com o Amazon SES

---
+++ src/config/mail/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'contato@mfcdev.com.br',
+      name: 'Marcelo Camillo',
+    },
+  },
+} as IMailConfig;
