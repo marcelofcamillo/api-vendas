97ae5388ec94b4db505952924bd1f2c36e4a3e47
Marcelo Camillo <marcelocamillo18@gmail.com>
2025-07-06 23:04:03 -0300

    Sistema de tratamento de erros

---
+++ src/common/infrastructure/http/middlewares/errorHandler.ts
@@ -0,0 +1,23 @@
+import { AppError } from '@/common/doamin/errors/app-error';
+import { NextFunction, Request, Response } from 'express';
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+}
