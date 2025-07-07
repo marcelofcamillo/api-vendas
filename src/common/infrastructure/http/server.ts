b24de0a6d24fe0ad38be1fc67419d7d15a4f95fa
Marcelo Camillo <marcelocamillo18@gmail.com>
2025-07-06 23:36:22 -0300

    Criação e configuração das variáveis de ambiente 2

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,6 @@
+import { env } from '../env';
 import { app } from './app';
 
-const port = 3333;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}. 🏆`);
 });
